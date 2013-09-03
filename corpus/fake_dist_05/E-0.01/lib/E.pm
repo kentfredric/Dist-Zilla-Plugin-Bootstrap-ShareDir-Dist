1791cf9bdff94997952237880f6e077c8807fd83
Kent Fredric <kentfredric@gmail.com>
2013-09-03 17:54:08 +1200

    Build results of c4bcf9c (on master)

--- corpus/fake_dist_05/E-0.01/lib/E.pm
+++ corpus/fake_dist_05/E-0.01/lib/E.pm
@@ -7,7 +7,12 @@ package E;
 # ABSTRACT: Fake dist stub
 
 use Moose;
+use File::ShareDir qw( dist_file );
+use Path::Tiny qw( path );
+
 with 'Dist::Zilla::Role::Plugin';
+
+our $content = path( dist_file('E','example.txt') )->slurp;
 
 1;
 
