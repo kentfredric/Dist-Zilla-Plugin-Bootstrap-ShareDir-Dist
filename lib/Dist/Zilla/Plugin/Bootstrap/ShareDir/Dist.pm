95f17fafab4b0573d3d43699ed106e44bf6fd63b
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:51:37 +1200

    v1.000002
     [00 Trivial]
     - CPANDAY!
     - no code change.
     - whitespace adjustment.
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000001, see misc/*.deps* for details
     - develop: +1 (suggests: ↑1)
    
     [Misc]
     - Improve source side POD to reduce build side whitespace.

--- lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Dist.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Dist.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Bootstrap::ShareDir::Dist;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 # ABSTRACT: Use a share directory on your dist during bootstrap
 
