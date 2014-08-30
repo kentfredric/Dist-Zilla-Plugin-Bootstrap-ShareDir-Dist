ff587ba26205acdbbb73179c649e2ef03a6c730c
Kent Fredric <kentfredric@gmail.com>
2014-08-31 01:50:54 +1200

    Build results of 6989577 (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Dist.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Dist.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Bootstrap::ShareDir::Dist;
 
-our $VERSION = '1.001000';
+our $VERSION = '1.001001';
 
 # ABSTRACT: Use a share directory on your dist during bootstrap
 
@@ -109,7 +109,7 @@ Dist::Zilla::Plugin::Bootstrap::ShareDir::Dist - Use a share directory on your d
 
 =head1 VERSION
 
-version 1.001000
+version 1.001001
 
 =head1 SYNOPSIS
 
